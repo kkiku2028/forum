18ec51e5601cb1d7a334d8972f4b119190b514f8
kkiku2028 <ghp_pvwykboero3ttjf3lcdnrvujkohjtg3la3rn>
2022-07-17 08:32:26 +0900

    Changes

---
+++ src/main/java/kkiku/forum/controller/ForumController.java
@@ -0,0 +1,13 @@
+package kkiku.forum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/forum")
+public class ForumController {
+    @RequestMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
